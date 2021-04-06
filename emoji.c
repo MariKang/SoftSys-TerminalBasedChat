5d7e0ad0b2eedb846de543c14ee6a27dc5dddcff
Jackie Zeng <jzeng@PaulRuvolos-MacBook-Pro.local>
2021-04-06 03:48:50 -0400

    finished emoji implementation

--- emoji.c
+++ emoji.c
@@ -1,94 +1,137 @@
+// C program to search and replace
+// all occurrences of a word with
+// other word.
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <regex.h> 
+#include <string.h>
 
-/* TODOS
-    - detect emoji using regex
-*/
+// Function to replace a string with another
+// string
+char* replaceWord(const char* s, const char* oldW,
+				const char* newW)
+{
+	char* result;
 
+	int i, cnt = 0;
+	int newWlen = strlen(newW);
+	int oldWlen = strlen(oldW);
 
-void get_emoji(char *flag){
-    printf("FLAG: %s\n", flag);
-    if(strstr(flag, ":BEAR:") != NULL){
-        printf("ʕ•́ᴥ•̀ʔっ\n");
-    }
-    if(strstr(flag, ":WHAT:") != NULL){
-        printf("(ㆆ_ㆆ)\n");
-    }
-    // case ":HUH:":
-    //   printf('(͡° ͜ʖ ͡°)');
+	// Counting the number of times old word
+	// occur in the string
+	for (i = 0; s[i] != '\0'; i++) {
+		if (strstr(&s[i], oldW) == &s[i]) {
+			cnt++;
+
+			// Jumping to index after the old word.
+			i += oldWlen - 1;
+		}
+	}
+
+	// Making new string of enough length
+	result = (char*)malloc(i + cnt * (newWlen - oldWlen) + 1);
+	i = 0;
+
+	while (*s) {
+		// compare the substring with the result
+		if (strstr(s, oldW) == s) {
+			strcpy(&result[i], newW);
+			i += newWlen;
+			s += oldWlen;
+		}
+		else
+			result[i++] = *s++;
+	}
+
+	result[i] = '\0';
+	return result;
 }
 
+char* get_emoji(char *flag){
+	char *emoji;
+    // printf("FLAG: %s\n", flag);
+    if(strstr(flag, "BEAR") != NULL){
+		emoji = "ʕ•́ᴥ•̀ʔっ";
+        // printf("ʕ•́ᴥ•̀ʔっ\n");
+    }
+    else if(strstr(flag, "WHAT") != NULL){
+		emoji = "(ㆆ_ㆆ)";
+        // printf("(ㆆ_ㆆ)\n");
+    }
+	else if(strstr(flag, "HUH") != NULL){
+		emoji = "(͡° ͜ʖ ͡°)";
+     	// printf("(͡° ͜ʖ ͡°)\n");
+    }
+	else{
+		emoji = "";
+	}
 
-//Prints the status of reg ex complilation
-void print_regex_complile_status(int return_val){
-    if (return_val == 0){
-        printf("Regular expression compiled successfully.\n");
-    }
-    else{
-        printf("Compilation error.\n");
-    }
+	return emoji;
 }
 
-// use regex to find the 
-
-void find_emoji_regex(){
-    char *msg = "jackie likes :BEAR: sdfhkjsdf";
-    regex_t find_emoji;
-    regmatch_t pmatch[1];
-    size_t     nmatch = 1;
-
-    int return_value = regcomp(&find_emoji, ":(.*?):", 0);
-    print_regex_complile_status(return_value);
-
-    if (0 == regexec(&find_emoji, "lol:BEAR:lol", 1, pmatch, 0)) {
-        
-      printf("With the whole expression, "
-             "a matched substring \"%.*s\" is found at position %d to %d.\n",
-             (int)(pmatch[0].rm_eo - pmatch[0].rm_so), &msg[pmatch[0].rm_so],
-             (int)(pmatch[0].rm_so), (int)(pmatch[0].rm_eo - 1));
-    } else {
-        printf("no match\n");
+void get_tokens(char *s, char *str_tokens[])
+{
+    int token_i = 0;
+    // return tokens and later we loop through all the tokens, and if it is a valid emoji, we replace it
+    char *start = s;
+    char *end = s;
+    while(*s) {
+        if(*s == '[') start = s;
+        else if(*s == ']') end = s;
+        if(start < end && *start) {
+              *end = 0;
+              str_tokens[token_i] = start+1;
+              start = s = end;
+              token_i++;
+        }
+        s++;
     }
-
-
-}
-
-
-void find_emoji_easy(){
-    char str[80] = "jackie likes :BEAR: sdfhkjsdf";
-    const char delim[3] = "::";
-    char *token;
-    
-    /* get the first token */
-    token = strtok(str, delim);
-    
-    /* walk through other tokens */
-    while( token != NULL ) {
-        printf( " %s\n", token );
-        
-        token = strtok(NULL, delim);
-    }
-}
-
-
-
-int main(){
-    // char str[20];
-    // printf("type an emoji:\n");
-    // if( fgets (str, sizeof(str), stdin)!=NULL ) {
-    // /* writing content to stdout */
+    str_tokens[token_i] = "";       // end empty string
+    // for (int i = 0; str_tokens[i][0] != '\0'; i++ ) {
+    //     printf("%s\n", str_tokens[i]);
     // }
-    // // printf("STRING: %s", str);
-    // get_emoji(str);
-
-    // find_emoji_regex();
-    find_emoji_easy();
-    // char *msg = "jackie likes :BEAR: sdfhkjsdf";
-    // char second_string[64];
-    // int n1 = sscanf(msg, ":(.*?):", second_string);
-    // printf("content: %s\n", second_string);
-    return 0;
+     
 }
+
+int main()
+{   
+	// char emoji[20];
+	const char *res = NULL;
+	char *str_tokens[100];
+    // char str[] = "(test 1 (test 2 3)foo(bar test) again)";
+    char msg[1000] = "jackie likes [BEAR] sdfhkjsdf [HUH]";
+	char c[100] = "[BEAR]";
+	char d[100] = "ʕ•́ᴥ•̀ʔっ";
+	char* result = NULL;
+
+	
+	char msg_copy[1000];		// create copy bc strtok modifies orig str
+	strcpy(msg_copy, msg);
+    get_tokens(msg_copy, str_tokens);		//modifies str_tokens
+
+
+    printf("INPUT: %s\n", msg);
+
+    for (int i = 0; str_tokens[i][0] != '\0'; i++ ) {
+		
+		//update emoji_text
+		strcpy(c, "[");
+		strcat(c, str_tokens[i]);
+		strcat(c, "]");
+		// printf("c: %s\n", c);
+
+		// update emoji
+		strcpy(d, get_emoji(str_tokens[i]));
+		// printf("d: %s\n", d);
+
+		// replace emoji text with emoji
+		strcpy(msg, replaceWord(msg, c, d));
+
+    }
+	printf("OUTPUT: %s\n", msg);
+
+
+	free(result);
+
+return 0;
+}
+
