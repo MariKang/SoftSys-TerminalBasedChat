7047794b5c084163224a2841133529b6ba08911d
Joon Kang <joonkang@Joons-MBP.olin.edu>
2021-04-05 23:22:36 -0400

    done

--- Apellatum.c
+++ Apellatum.c
@@ -93,12 +93,41 @@ void *read_msg(void *arguments){
 		puts(args -> buf);
 	}
 }
+// Returns hostname for the local computer
+void checkHostName(int hostname)
+{
+    if (hostname == -1)
+    {
+        perror("gethostname");
+        exit(1);
+    }
+}
+
+// Returns host information corresponding to host name
+void checkHostEntry(struct hostent * hostentry)
+{
+    if (hostentry == NULL)
+    {
+        perror("gethostbyname");
+        exit(1);
+    }
+}
+
+// Converts space-delimited IPv4 addresses
+// to dotted-decimal format
+void checkIPbuffer(char *IPbuffer)
+{
+    if (NULL == IPbuffer)
+    {
+        perror("inet_ntoa");
+        exit(1);
+    }
+}
 
 
 
 int main(int argc, char *argv[]){
 
-	int src_hst;
 	if(catch_signal(SIGINT, handle_shutdown) == -1)
 		error("Can't set interrupt handler");
 
@@ -112,18 +141,25 @@ int main(int argc, char *argv[]){
 	unsigned int address_size = sizeof(client_addr);
 
 	puts("Welcome to the Apellatum Messenger.\n");
+  char hostbuffer[256];
+    char *IPbuffer;
+    struct hostent *host_entry;
+    int hostname;
 
+    // To retrieve hostname
+    hostname = gethostname(hostbuffer, sizeof(hostbuffer));
+    checkHostName(hostname);
 
-	if(gethostname(src_host, sizeof(src_host)) < 0)  {
-		printf("Error in getting host name...\n");
-		return 0;
-	} else{
-		for(int idx=0; src_hst-> h_addr_list[idx];idx++)
-		{
-			puts("Your IP Address is");
-			puts(inet_ntoa((*(struct in_addr *) src_hst-> h_addr_list[idx])));
-		}
-	}
+    // To retrieve host information
+    host_entry = gethostbyname(hostbuffer);
+    checkHostEntry(host_entry);
+
+    // To convert an Internet network
+    // address into ASCII string
+    IPbuffer = inet_ntoa(*((struct in_addr*)
+                           host_entry->h_addr_list[0]));
+
+    printf("Host IP: %s \n", IPbuffer);
 
 	puts("Waiting for connection...");
 	char msg[1000];
